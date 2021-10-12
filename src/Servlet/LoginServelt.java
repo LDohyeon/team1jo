a6ec3c21cf9b62b836fd581fb1fd0a1c936ddfc6
park-jung-hyun-92 <shg99146@naver.com>
2021-10-12 16:09:43 +0900

    로그인 / 정현

--- src/Servlet/LoginServelt.java
+++ src/Servlet/LoginServelt.java
@@ -13,17 +13,12 @@ import DTO.MemberDTO;
 @WebServlet("/login.do")
 public class LoginServelt extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.setAttribute("loginMsg", "아이디 또는 비밀번호가 일치하지 않습니다");
-		
 		RequestDispatcher dispatcher= request.getRequestDispatcher("login.jsp");
 		dispatcher.forward(request, response);
 	}
-
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String url="index.jsp";//���� main.jsp ��� index.jsp�� ��ü 
@@ -38,16 +33,18 @@ public class LoginServelt extends HttpServlet {
 		MemberDTO mDTO = mDAO.loginMember(id, pw);
 		
 		if(mDTO == null)
-		{
-			// 로그인 실패시
-			url="login.do";
+		{	
+			// 로그인 실패시			url="login.jsp"; 
+			request.setAttribute("loginMsg", "아이디 또는 비밀번호가 일치하지 않습니다");
 		}
 		else
 		{	
+			// 로그인 성공시
 			HttpSession session= request.getSession();
-			session.setAttribute("loginUser", mDTO);//로그인 성공시
+			session.setAttribute("loginUser", mDTO); 
 			session.setAttribute("loginUserId", mDTO.getId());
 		}		
-		response.sendRedirect(url);
+		RequestDispatcher dispatcher= request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
 	}
 }
