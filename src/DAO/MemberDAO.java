c0de8196b7c8f101124f8b8d7ea89c06110ee724
student <student@IOT10>
2021-10-14 14:55:07 +0900

    정현씨에게/ 도현

--- src/DAO/MemberDAO.java
+++ src/DAO/MemberDAO.java
@@ -382,7 +382,7 @@ public class MemberDAO {
 			
 			pstmt.setString(1, ids);
 			pstmt.setString(2, names);
-<<<<<<< HEAD
+
 			rs=pstmt.executeQuery();
 			
 			while(rs.next())
@@ -411,61 +411,7 @@ public class MemberDAO {
 	}
 	
 	
-	public List<MemberDTO> memberSerachList(String id, String name, String authority)
-	{
-		String ids=id+"%";
-		String names=name+"%";
-		
-		List<MemberDTO> list = new ArrayList<MemberDTO>();
-		
-		String sql="select * from member where id like ? and authority=? or name like ? and authority=?";
-		
-		Connection conn =null;
-		PreparedStatement pstmt =null;
-		ResultSet rs=null;
-		
-		
-		try
-		{
-			conn= getConnection();
-			pstmt= conn.prepareStatement(sql);
-			
-			pstmt.setString(1, ids);
-			pstmt.setString(2, authority);
-			pstmt.setString(3, names);
-			pstmt.setString(4, authority);
-			
-			System.out.println("mem : "+ pstmt);
 
-=======
-			
-			rs=pstmt.executeQuery();
-			
-			while(rs.next())
-			{
-				MemberDTO mDTO = new MemberDTO();
-				
-				mDTO.setNum(rs.getInt("num"));
-				mDTO.setId(rs.getString("id"));
-				mDTO.setName(rs.getString("name"));
-				mDTO.setEmail(rs.getString("email"));
-				mDTO.setAuthority(rs.getString("authority"));
-				
-				list.add(mDTO);
-			}
-		}
-		catch(Exception e)
-		{
-			System.out.println("회원 관리 검색 출력 실패"+e);
-		}
-		finally
-		{
-			close(conn, pstmt, rs);
-		}
-
-		return list;
-	}
-	
 	
 	public List<MemberDTO> memberSerachList(String id, String name, String authority)
 	{
@@ -493,7 +439,6 @@ public class MemberDAO {
 			
 			System.out.println("mem : "+ pstmt);
 			
->>>>>>> branch 'member' of https://github.com/LDohyeon/team1jo.git
 			rs=pstmt.executeQuery();
 			
 			while(rs.next())
