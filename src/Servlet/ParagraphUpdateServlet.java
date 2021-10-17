5f7e1bd206ffb3206b061e0d7f075f886876545c
하영 <@222.111.21.43>
2021-10-17 20:35:32 +0900

    게시글 수정 값 넘어오는 거 확인 / 하영

--- src/Servlet/ParagraphUpdateServlet.java
+++ src/Servlet/ParagraphUpdateServlet.java
@@ -2,9 +2,13 @@ package Servlet;
 
 import java.io.*;
 
+
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
+
+import DAO.ParagraphDAO;
+import DTO.ParagraphDTO;
 
 
 
@@ -13,10 +17,25 @@ public class ParagraphUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		RequestDispatcher dispatcher=request.getRequestDispatcher("paragraphUpdate.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String title=request.getParameter("title");
+		String content=request.getParameter("content");
+		
+		System.out.println("update title: "+title);
+		System.out.println("update content: "+content);
+		
+		ParagraphDTO pDTO=new ParagraphDTO();
+		pDTO.setTitle(title);
+		pDTO.setContents(content);
+		
+		ParagraphDAO pDAO=ParagraphDAO.getInstance();
+		
+		pDAO.paragraphUpdate(pDTO);
 
 	}
 
