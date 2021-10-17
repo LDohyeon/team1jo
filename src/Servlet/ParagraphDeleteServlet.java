321146dec8a1a6fb5b69c869ec20da495ce01824
하영 <@222.111.21.43>
2021-10-17 20:42:59 +0900

    삭제 게시글 번호 넘어오는 거 확인 / 하영

--- src/Servlet/ParagraphDeleteServlet.java
+++ src/Servlet/ParagraphDeleteServlet.java
@@ -6,6 +6,11 @@ import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
 
+import DAO.MemberDAO;
+import DAO.ParagraphDAO;
+import DTO.ParagraphDTO;
+
+
 
 
 @WebServlet("/paragraphDelete.do")
@@ -13,7 +18,18 @@ public class ParagraphDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		int num=Integer.parseInt(request.getParameter("num"));
 
+		
+		ParagraphDTO pDTO=new ParagraphDTO();
+		pDTO.setNum(num);
+		
+		ParagraphDAO pDAO=ParagraphDAO.getInstance();
+		pDAO.paragraphDelete(pDTO.getNum());
+		
+		System.out.println("paragraphDelete Num: "+num);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
