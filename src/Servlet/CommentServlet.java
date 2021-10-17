e4ce647ef11265415703427dc3439518b4ffd630
하영 <@222.111.21.43>
2021-10-17 20:44:18 +0900

    댓글 넘어오는 거 확인

---
+++ src/Servlet/CommentServlet.java
@@ -0,0 +1,31 @@
+package Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.ParagraphDAO;
+import DTO.MemberDTO;
+import DTO.ParagraphDTO;
+
+
+@WebServlet("/comment.do")
+public class CommentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String content=request.getParameter("content");
+		
+		System.out.println("comment content: "+content);
+	}
+
+}
