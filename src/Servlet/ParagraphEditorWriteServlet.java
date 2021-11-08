2501a40b1b80554ea6fd00e297e47d3fd9c9c149
student <student@IOT10>
2021-11-08 15:04:21 +0900

    수정 / 도현

--- src/Servlet/ParagraphEditorWriteServlet.java
+++ src/Servlet/ParagraphEditorWriteServlet.java
@@ -37,8 +37,6 @@ public class ParagraphEditorWriteServlet extends HttpServlet {
 		String category = "질문";
 		String language="";
 		
-		
-		
 		String[] contents=content.split("※");
 		Map<String, String> map = new HashMap<String, String>();
 
